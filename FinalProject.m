9602bdb022fa5a3a15fd419aa46d8d020fc48add
ktmeineriu <152181172+ktmeineriu@users.noreply.github.com>
2023-11-27 16:43:35 -0500

    Add files via upload

--- FinalProject.m
+++ FinalProject.m
@@ -23,3 +23,53 @@ D = distance_bin(total_network.a);
 
 % calculate binary path length and efficiency
 [lambda,efficiency] = charpath(D); %Having some issues running this
+
+%%
+
+% run modularity maximization for a network where all edge weights are positive
+[Ci,Q] = community_louvain(total_network.a);
+
+%%
+
+% number of times to repeat community detection algorithm
+num_iter = 1; %reduced to 10 but still said out of memory
+
+% number of nodes
+n_nodes = length(total_network.a);
+
+% empty array for storing the community labels
+Ci = zeros(n_nodes,num_iter);
+
+% run the community detection algorithm num_iter times
+for iter = 1:num_iter
+  Ci(:,iter) = community_louvain(total_network.a);
+end
+%%
+
+% calculate the module coassignment matrix -- for every pair of nodes
+% how many times were they assigned to the same community
+Coassignment = agreement(Ci)/num_iter;
+
+% node we use the consensus clustering function
+thr = 0.5;
+cicon = consensus_und(Coassignment,thr,num_iter);
+
+%%
+%Get the following errors from the above section if iterator is 10 or 50:
+
+% Out of memory.
+% 
+% Error in dummyvar (line 6)
+%         I = [I,double(ci(:,i) == j)];
+% 
+% Error in agreement (line 36)
+%     ind = dummyvar(ci);
+% 
+% Error in FinalProject (line 50)
+% Coassignment = agreement(Ci)/num_iter;
+% 
+% Related documentation
+%%
+%However if only do 1 as num_iter it works however cicon is just the
+%numbers as is. It does not provide information about the network. Need to
+%be able to tease out more information or specify for a directed network.
